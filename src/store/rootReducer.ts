5290a58c4184fab857f94bc7c77cdb3f4e87feee
lelderbe <fvdc@ya.ru>
2024-05-19 10:03:00 +0300

    refactor: cleanup

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
-import { userSlice } from './slices/user-slice';
 import { authSlice } from './slices/auth-slice';
 import { filtersSlice } from './slices/filters-slice';
 import { api } from './api/api';
 
 export const rootReducer = combineReducers({
-	[userSlice.name]: userSlice.reducer,
 	[authSlice.name]: authSlice.reducer,
 	[api.reducerPath]: api.reducer,
 	[filtersSlice.name]: filtersSlice.reducer,
